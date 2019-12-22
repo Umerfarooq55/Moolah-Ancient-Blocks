0246706d172a21383a6df7c53ce0f8079bc775e9
--replace-all <umerfarooqsaeed7@gmail.com>
2019-12-22 13:07:26 +0500

    Login Screen and Backbutton

--- app/src/main/java/com/moolahmobile/moolahancientblocks/IntroActivity.kt
+++ app/src/main/java/com/moolahmobile/moolahancientblocks/IntroActivity.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_intro.*
 
 class IntroActivity : AppCompatActivity() {
-
+    override fun onBackPressed() {
+        
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
